80f076a174e6544b3885db40558ab2abb2d8a8a8
CTW - Samir Ayoub (CTW) <Samir.Ayou@ctw.bmwgroup.com>
2022-07-13 23:26:34 +0100

    feature/nasa-project: unit tests fix

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,15 @@
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const {
+  loadPlanetsData,
+} = require('../../models/planets.model');
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
-  });
+    await loadPlanetsData();
+    });
 
   afterAll(async () => {
     await mongoDisconnect();
@@ -22,36 +26,36 @@ describe("Launches API", () => {
 
   describe("Test POST /launch", () => {
     const completeLaunchData = {
-      mission: "US Enterprise",
-      rocket: "NCC 1312-D",
-      destination: "Kepler-62-F",
-      launchDate: "January 4, 2028",
+      mission: 'US Enterprise',
+      rocket: 'NCC 1312-D',
+      destination: 'Kepler-62 f',
+      launchDate: 'January 4, 2028',
     };
 
     const launchDataWithoutDate = {
       mission: "US Enterprise",
       rocket: "NCC 1312-D",
-      destination: "Kepler-62-F",
+      destination: "Kepler-62 f",
     };
 
     const launchDataWithInvalidDate = {
       mission: "US Enterprise",
       rocket: "NCC 1312-D",
-      destination: "Kepler-62-F",
+      destination: "Kepler-62 f",
       launchDate: "wow",
     };
 
-    test("It should respond with 201 created", async () => {
+    test('It should respond with 201 created', async () => {
       const response = await request(app)
-        .post("/v1/launches")
+        .post('/v1/launches')
         .send(completeLaunchData)
-        .expect("Content-Type", /json/)
+        .expect('Content-Type', /json/)
         .expect(201);
-
+  
       const requestDate = new Date(completeLaunchData.launchDate).valueOf();
       const responseDate = new Date(response.body.launchDate).valueOf();
-
       expect(responseDate).toBe(requestDate);
+  
       expect(response.body).toMatchObject(launchDataWithoutDate);
     });
 
